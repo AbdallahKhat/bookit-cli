16c2b748063e3ecab2dbe268b49252c8958b078a
Abdallah Khater <abdallah.khater.eng@gmail.com>
2025-10-12 01:13:27 +0300

    test(command-parser): add basic main test for command word parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,52 @@
+#include "CommandInfo.h"
+#include "CommandParser.h"
 #include <iostream>
-#include "LibraryManager.h"
 
-int main()
+void tryHelpStr()
 {
-    std::cout << "Hello, BookIt Project!" << std::endl;
+    std::cerr << "Try 'bookit --help' for more infomration.\n";
+}
+
+// prints error message.
+void printError(CommandInfo::ParseError error, char* argv[])
+{
+    using namespace CommandInfo;
+
+    switch (error)
+    {
+    case MissingCommand:
+        std::cerr << "bookit: Missing command. \n";
+        tryHelpStr();
+        return;
+
+    case UnknownCommand:
+        std::cerr << "bookit: Invalid command: " << '\'' << argv[1] << "\'\n";
+        tryHelpStr();
+        return;
+    case MissingArgument:
+    case InvalidOption:
+    case InvalidPath:
+    default:
+        std::cerr << "An unexpected error has occurred.\n";
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    // std::cout << "Hello, BookIt Project!" << '\n';
+
+    CommandParser parsedCmd{argc, argv};
+
+    if (!parsedCmd.isValid())
+    {
+        CommandInfo::ParseError err = parsedCmd.getError(); // get error code
+
+        printError(err, argv);
+
+        return err;
+    }
+
+    std::cout << "The command given is: " << parsedCmd.getCmdStr() << '\n';
 
     return 0;
 }
