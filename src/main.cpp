4577043e7ae93339f029ff3049e04c26acd7f63a
Abdallah Khater <abdallah.khater.eng@gmail.com>
2025-10-15 01:00:26 +0300

    test(command-parser): verify parser handles all valid and invalid argument patterns
    parser tests covering commands, paths, and options

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,22 @@
 #include "CommandInfo.h"
 #include "CommandParser.h"
 #include <iostream>
-
-void tryHelpStr()
-{
-    std::cerr << "Try 'bookit --help' for more infomration.\n";
-}
-
-// prints error message.
-void printError(CommandParser::Error error)
-{
-    using namespace CommandInfo;
-    
-    const auto& arg = error.invalidArg;
-    switch (error.type)
-    {
-    case MissingCommand:
-        std::cerr << "bookit: missing command\n";
-        break;
-    case UnknownCommand:
-        std::cerr << "bookit: invalid command " << '\'' << arg << "\'\n";
-        break;
-    case MissingArgument:
-        std::cerr << "bookit: Missing file operand\n";
-        break;
-    case InvalidPath:
-        std::cerr << "bookit: invalid path argument '" << arg << "'\n";
-        break;
-    case NotAFile:
-        std::cerr << "bookit: target '" << arg << "' is not a file\n";
-        break;
-    case NotADirectory:
-        std::cerr << "bookit: target '" << arg << "' is not a directory\n";
-        break;
-    case UnexpectedArgs:
-        std::cerr << "bookit: unexpected argument '" << arg << "\'\n";
-        break;
-    case InvalidOption:
-    default:
-        std::cerr << "bookit: an unexpected error has occurred\n";
-    }
-    tryHelpStr();
-}
+#include <string_view>
 
 int main(int argc, char* argv[])
 {
-    // std::cout << "Hello, BookIt Project!" << '\n';
-
     CommandParser parsedCmd{argc, argv};
 
     if (!parsedCmd.isValid())
     {
-        CommandParser::Error error = parsedCmd.error(); // get error code
-
-        printError(error);
-
-        return error.type;
+        auto [errorType, arg] = parsedCmd.error(); // get error code
+        CommandInfo::printError(errorType, arg);
+        return errorType;
+    }
+    if (parsedCmd.isHelp())
+    {
+        CommandInfo::printHelp();
+        return 0;
     }
 
     std::cout << "The command given is: " << CommandInfo::toString(parsedCmd.commandType()) << '\n';
@@ -64,6 +25,11 @@ int main(int argc, char* argv[])
         std::cout << "The Directory given is: " << parsedCmd.path() << '\n';
     }
     else { std::cout << "The path to file given is: " << parsedCmd.path() << '\n'; }
+
+    for (const auto& [option, value] : parsedCmd.options())
+    {
+        std::cout << "Option: " << option << " | Value: " << value << '\n';
+    }
 
     return 0;
 }
