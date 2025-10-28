4fdd8223b09dcb8bd55fd0447f06926459060f12
Abdallah Khater <abdallah.khater.eng@gmail.com>
2025-10-28 08:29:51 +0300

    feat(library-manager): add command execution and workspace initialization command skeleton

--- include/LibraryManager.h
+++ include/LibraryManager.h
@@ -1,11 +1,25 @@
 #ifndef LIBRARYMANAGER_H
 #define LIBRARYMANAGER_H
 
+#include "CommandParser.h"
+
 // LibraryManager is the coordinator class for all features of the library.
 // It serves as the main interface for library operations.
 class LibraryManager
 {
+public:
+    // Constructors
+    LibraryManager(const CommandParser& parsedCmd) noexcept : m_parsedCmd{parsedCmd} {}
 
+    // Public Interfaces
+    bool ExecuteCmd();
+
+private:
+    // Private facade command functions
+    bool InitWorspaceEnv();
+
+    // Member variables
+    const CommandParser& m_parsedCmd;
 };
 
 #endif // LIBRARYMANAGER_H
--- src/LibraryManager.cpp
+++ src/LibraryManager.cpp
@@ -1,1 +1,75 @@
 #include "LibraryManager.h"
+#include "CommandInfo.h"
+#include "CommandParser.h"
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+// static helper function prototypes
+static bool verifyDirectoryReady(const fs::path& path);
+
+/**
+ *
+ * Public Interface Function implementations
+ *
+ */
+
+bool LibraryManager::ExecuteCmd()
+{
+    switch (m_parsedCmd.commandType())
+    {
+    case CommandInfo::Init:
+        InitWorspaceEnv();
+        break;
+
+    // case CommandInfo::AddBook:
+    default:
+        break;
+    }
+
+    return true;
+}
+
+/**
+ *
+ * Private facade command function implementations
+ *
+ */
+
+// Initialize a new Workspace Enironment for library use
+bool LibraryManager::InitWorspaceEnv()
+{
+    const auto& path = m_parsedCmd.path();
+
+    if (!verifyDirectoryReady(path)) { return false; }
+
+    // TODO: Execute Concrete Command (CommandInit)
+
+    return true;
+}
+
+/**
+ *
+ * Static helper functions for the command functions
+ *
+ */
+
+/* Helpers for path verifications */
+
+// checks if path refers to real directory
+static bool verifyDirectoryReady(const fs::path& path)
+{
+    if (!fs::is_directory(path))
+    {
+        // TODO: Error: does not refer to a real directory
+        return false;
+    }
+
+    if (!fs::is_empty(path))
+    {
+        // TODO: Error: directory must be empty to initialize
+        return false;
+    }
+
+    return true;
+}
