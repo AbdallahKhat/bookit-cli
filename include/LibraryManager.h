4a4678a15dc2fe12b83c841848b74718c353065d
Abdallah Khater <abdallah.khater.eng@gmail.com>
2025-11-15 17:39:06 +0200

    feat(library-manager): unify command execution flow and add addbook handling

--- include/LibraryManager.h
+++ include/LibraryManager.h
@@ -3,6 +3,7 @@
 
 #include "Command.h"
 #include "CommandParser.h"
+#include "Workspaces.h"
 
 // LibraryManager is the coordinator class for all features of the library.
 // It serves as the main interface for library operations.
@@ -16,10 +17,8 @@ public:
     bool executeCmd();
 
 private:
-    // Private facade command functions
-    bool initWorspaceEnv();
-
-    std::unique_ptr<Command> createCommand();
+    // Private functions
+    std::unique_ptr<Command> createCommand(const Bookit::Workspaces& workspace);
 
     // Member variables
     const CommandParser& m_parsedCmd;
